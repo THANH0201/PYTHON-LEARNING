003918cc7a1a9a2de71a3288f8dc3ef67f8fbccc
THANH0201 <160411131+THANH0201@users.noreply.github.com>
2024-08-23 15:46:42 +0700

    EXERCISE MODULE 2

--- MODULE 2.py
+++ MODULE 2.py
@@ -2,3 +2,7 @@
 import math
 import random
 #Exercise 1
+# Write a program that asks your name and then greets you by your name: Examples:
+# If you enter Viivi as your name, the program will greet you with `Hello, Viivi!`.
+# If you enter Ahmed as your name, the program will greet you with `Hello, Ahmed!`.
+print(f"Helo, {input("Enter your name here please:")}!")
