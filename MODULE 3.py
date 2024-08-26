95ccc5fe04476f8ef96eb17c64bfd5269194f233
THANH0201 <160411131+THANH0201@users.noreply.github.com>
2024-08-26 18:45:41 +0700

    EXERCISE MODULE 3

--- MODULE 3.py
+++ MODULE 3.py
@@ -54,3 +54,4 @@ else: print("Please enter the gender as CAPSLOCK text")
 #Write a program that asks the user to enter a year and notifies the user whether the input year is a leap year.
 #A year is a leap year if it is divisible by four. However, years divisible by 100 are leap years only if they are
 #also divisible by 400.
+qqqq
